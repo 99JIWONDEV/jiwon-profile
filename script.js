69d3d063b083a53f0b71d3d4cb82e7f9619bdba1
99JIWONDEV <99jiwonpark@gmail.com>
2025-02-12 22:54:20 +0900

    [comment] 주석 수정

--- script.js
+++ script.js
@@ -14,22 +14,28 @@ const navbar = document.querySelector("[data-navbar]");
 const navLinks = document.querySelectorAll("[data-nav-link]");
 const navToggler = document.querySelector("[data-nav-toggler]");
 
+// 네비게이션 바 토글 함수
 const toggleNavbar = function () {
 	navbar.classList.toggle("active");
 	navToggler.classList.toggle("active");
 };
 
+// 네비게이션 토글 버튼에 클릭 이벤트 추가
 addEventOnElem(navToggler, "click", toggleNavbar);
 
+// 네비게이션 바 닫기 함수
 const closeNavbar = function () {
 	navbar.classList.remove("active");
 	navToggler.classList.remove("active");
 };
 
+// 네비게이션 링크에 클릭 이벤트 추가
 addEventOnElem(navLinks, "click", closeNavbar);
 
+// 헤더 요소 선택
 const header = document.querySelector("[data-header]");
 
+// 스크롤 이벤트에 따라 헤더 클래스 추가/제거
 window.addEventListener("scroll", function () {
 	if (window.scrollY > 100) {
 		header.classList.add("active");
